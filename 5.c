e5e7b3ef3028b2196ccd19c5cd39f8208a12471d
evos456 <161448262+evos456@users.noreply.github.com>
2024-06-28 05:49:29 +0530

    Update 5.c

--- 5.c
+++ 5.c
@@ -1,48 +1,93 @@
 #include <stdio.h>
-#define infinity 9999
-#define MAX 20
-int minimum(int a,int b)
-{
-	if(a<=b)
-	  return a;
-	else
-	  return b;
+#define INFINITY 9999
+#define MAX 10
+
+void dijkstra(int G[MAX][MAX], int n, int startnode);
+
+int main() {
+    int G[MAX][MAX], i, j, n, startnode;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter the adjacency matrix:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &G[i][j]);
+        }
+    }
+
+    printf("Enter the starting node: ");
+    scanf("%d", &startnode);
+
+    dijkstra(G, n, startnode);
+
+    return 0;
 }
-main()
-{
-	int i,j,k,n,start,end,adj[MAX][MAX],path[MAX][MAX];
-	printf("Enter number of vertices : ");
-	scanf("%d",&n);
-	printf("Enter weighted matrix :\n");
-	for(i=0;i<n;i++)
-	   for(j=0;j<n;j++)
-		scanf("%d",&adj[i][j]);
-       for(i=0;i<n;i++)
-	   for(j=0;j<n;j++)
-	   if(adj[i][j]==0)
-		path[i][j]=infinity;
-	   else
-		path[i][j]=adj[i][j];
-	for(k=0;k<n;k++)
-	{
-		for(i=0;i<n;i++)
-		  for(j=0;j<n;j++){
-            if(i==j)
-            path[i][j]=infinity;
-          else
-		    path[i][j]=minimum(path[i][j],path[i][k]+path[k][j]);
-	}
-	}
-	printf("Shortest path matrix is :\n");
-	for(i=0;i<n;i++)
-	{
-		for(j=0;j<n;j++)
-			printf("%6d",path[i][j]);
-		printf("\n");
-	}
-	printf("Enter start vertex :");
-	scanf("%d",&start);
-	printf("Enter end vertex :");
-	scanf("%d",&end);
-	printf("the min. cost between %d and %d is %d",start,end,path[start][end]);
+
+void dijkstra(int G[MAX][MAX], int n, int startnode) {
+    int cost[MAX][MAX], distance[MAX], pred[MAX];
+    int visited[MAX], count, mindistance, nextnode, i, j;
+
+    // Create the cost matrix
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (G[i][j] == 0) {
+                cost[i][j] = INFINITY;
+            } else {
+                cost[i][j] = G[i][j];
+            }
+        }
+    }
+
+    // Initialize distance and pred arrays
+    for (i = 0; i < n; i++) {
+        distance[i] = cost[startnode][i];
+        pred[i] = startnode;
+        visited[i] = 0;
+    }
+
+    distance[startnode] = 0;
+    visited[startnode] = 1;
+    count = 1;
+
+    while (count < n - 1) {
+        mindistance = INFINITY;
+
+        // Find the next node with the minimum distance
+        for (i = 0; i < n; i++) {
+            if (distance[i] < mindistance && !visited[i]) {
+                mindistance = distance[i];
+                nextnode = i;
+            }
+        }
+
+        visited[nextnode] = 1;
+
+        // Update the distances of the adjacent nodes
+        for (i = 0; i < n; i++) {
+            if (!visited[i]) {
+                if (mindistance + cost[nextnode][i] < distance[i]) {
+                    distance[i] = mindistance + cost[nextnode][i];
+                    pred[i] = nextnode;
+                }
+            }
+        }
+
+        count++;
+    }
+
+    // Print the distance and path
+    for (i = 0; i < n; i++) {
+        if (i != startnode) {
+            printf("\nDistance from node %d to node %d is %d", startnode, i, distance[i]);
+            printf("\nPath = %d", i);
+
+            j = i;
+            do {
+                j = pred[j];
+                printf(" <- %d", j);
+            } while (j != startnode);
+        }
+    }
 }
