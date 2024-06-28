23c72b476078f8028e4ee4996da49f482a3ac361
evos456 <161448262+evos456@users.noreply.github.com>
2024-06-28 06:47:31 +0530

    Update 6.c#include <stdio.h>  int a[10][10], n;  void adj(int k);  int main() {     int i, j, root;     printf("Enter number of nodes: ");     scanf("%d", &n);      printf("Enter adjacency matrix\n");     for (i = 1; i <= n; i++) {         for (j = 1; j <= n; j++) {             printf("Enter connection of %d --> %d: ", i, j);             scanf("%d", &a[i][j]);         }     }      printf("Enter root node: ");     scanf("%d", &root);      adj(root);      return 0; }  void adj(int k) {     int j;      printf("Adjacent nodes of root node %d:\n", k);     for (j = 1; j <= n; j++) {         if (a[k][j] == 1) {             printf("%d\t", j);         }     }     printf("\n");      printf("Nodes not adjacent to root node %d:\n", k);     for (j = 1; j <= n; j++) {         if (a[k][j] == 0 && j != k) {             printf("%d\t", j);         }     }     printf("\n"); }

--- 6.c
+++ 6.c
@@ -1,31 +1,46 @@
-#include<stdio.h>
-int a[10][10],n;
-int main(){
-int i,j,root;
-printf("Enter no. of nodes");
-scanf("%d",&n);
-printf("Enter adjacent matrix\n");
-for(i=1;i<=n;i++){
-for(j=1;j<=n;j++){
- printf("Enter connectin of %d-->%d:",i,j);
- scanf("%d",&a[i][j]);
+#include <stdio.h>
+
+int a[10][10], n;
+
+void adj(int k);
+
+int main() {
+    int i, j, root;
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix\n");
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
+            printf("Enter connection of %d --> %d: ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Enter root node: ");
+    scanf("%d", &root);
+
+    adj(root);
+
+    return 0;
 }
-}
-printf("Enter root node:");
-scanf("%d",&root);
-adj(root);
-}
-adj(int k){
-int i,j;
-printf("Adajacent node of root node:\n");
-printf("%d\n\n",k);
-for(j=1;j<=n;j++){
-    if(a[k][j]==1 || a[i][k]==1)
-        printf("%d\t",j);
-}
-printf("\n");
-for(i=1;i<=n;i++){
-if((a[k][j]==0) && (a[i][k]==0) &&(i!=k))
-    printf("%d",i);
-}
+
+void adj(int k) {
+    int j;
+
+    printf("Adjacent nodes of root node %d:\n", k);
+    for (j = 1; j <= n; j++) {
+        if (a[k][j] == 1) {
+            printf("%d\t", j);
+        }
+    }
+    printf("\n");
+
+    printf("Nodes not adjacent to root node %d:\n", k);
+    for (j = 1; j <= n; j++) {
+        if (a[k][j] == 0 && j != k) {
+            printf("%d\t", j);
+        }
+    }
+    printf("\n");
 }
